d2a959d80dca8a9f2bf4f26da13a322fda24094a
Yuliya Masechko <yulia.masechko@gmail.com>
2024-09-08 17:08:35 +0300

    Fixed bugs

--- index.js
+++ index.js
@@ -13,20 +13,40 @@ const foo = async() => {
         let pathInsideDir = path.join(pathToDir, 'folder' + i);
         await fsPromises.mkdir(path.join(pathToDir, 'folder' + i), {recursive: true});
         // await fsPromises.rm(path.join(pathToDir, 'folder' + i), {recursive: true}
-        const stat = await fsPromises.stat(pathInsideDir);
-        console.log(pathInsideDir);
-        console.log(stat.isDirectory()? 'folder' + i + ': IT IS A FOLDER' : 'folder' + i + ': IT IS A FILE');
+
+        // const stat = await fsPromises.stat(pathInsideDir);
+        // console.log(pathInsideDir);
+        // console.log(stat.isDirectory()? 'folder' + i + ': IT IS A FOLDER' : 'folder' + i + ': IT IS A FILE');
         for (let j= 1; j<=5; j++) {
             const filePath = path.join(pathInsideDir, `file${j}.txt`);
             await fsPromises.writeFile(filePath, 'Hello World!' );
-            const stat = await fsPromises.stat(filePath);
-            console.log(filePath);
-            console.log(stat.isFile()? `file${j}.txt` + ':IT IS A FILE' : `file${j}.txt` + ':IT IS A FOLDER');
+
+            // const stat = await fsPromises.stat(filePath);
+            // console.log(filePath);
+            // console.log(stat.isFile()? `file${j}.txt` + ':IT IS A FILE' : `file${j}.txt` + ':IT IS A FOLDER');
         }
     }
 
    const data = await fsPromises.readFile(pathToFile, 'utf-8');
-    console.log(data);
+   console.log(data);
+
+    const dataRead = await fsPromises.readdir(pathToDir);
+    for (const folder of dataRead) {
+        const pathFolder = path.join(pathToDir, folder);
+        const stat = await fsPromises.stat(pathFolder);
+        console.log(pathFolder);
+        console.log(stat.isDirectory()? `${folder}: IT IS A FOLDER` : `${folder}: IT IS A FILE`);
+
+        const dataFolder = await fsPromises.readdir(pathFolder);
+        for (const file of dataFolder) {
+            const pathFile = path.join(pathFolder, file);
+            const stat = await fsPromises.stat(pathFile);
+            console.log(pathFile);
+            console.log(stat.isFile()? `${file}` + ': IT IS A FILE' : `${file}` + ': IT IS A FOLDER');
+        }
+    }
+
+
 };
 
 void foo();
